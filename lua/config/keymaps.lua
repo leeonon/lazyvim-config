d1ebc7dd0cb3a6358018809f9f436c65c40ab4bf
leeonon <v5sollee@gmail.com>
2023-12-06 09:47:46 +0800

    feat: change config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- keymap在VeryLazy事件上自动加载
 -- 默认的键映射总是设置:https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- 在这里添加任何额外的键映射
+--
+local keymap = vim.keymap
+
+keymap.set("i", "jk", "<Esc>")
+
